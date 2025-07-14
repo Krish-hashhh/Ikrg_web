bd4e27cc2301c17fc9f8069c5876dc0108fd46b9
NavyaGupta0301 <f20240850@pilani.bits-pilani.ac.in>
2025-07-14 21:30:47 +0530

    Added pics of vehicles and projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Projects.css';
+import p1 from '../Assets/p1.jpeg';
+import p2 from '../Assets/p2.jpeg';
+import p3 from '../Assets/p3.jpeg';
 
 export default function Projects() {
   return (
@@ -8,6 +11,16 @@ export default function Projects() {
       <div className="project-grid">
         {[1, 2, 3].map(num => (
           <div key={num} className="project-card">
+            <div className="project-image">
+              <img
+                src={
+                  num === 1
+                    ? p1
+                    : num === 2
+                      ? p2
+                      : p3
+                }></img>
+            </div>
             <h3>Project {num}</h3>
             <p>Details about the project {num}.</p>
           </div>
--- src/Components/Vehicles.jsx
+++ src/Components/Vehicles.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Vehicles.css';
+import v1 from '../Assets/v1.jpeg';
+import v2 from '../Assets/v2.jpeg';
+import v3 from '../Assets/v3.jpeg';
 
 export default function Vehicles() {
   return (
@@ -8,7 +11,16 @@ export default function Vehicles() {
       <div className="vehicle-grid">
         {[1, 2, 3].map(num => (
           <div key={num} className="vehicle-card">
-            <div className="vehicle-image"></div>
+            <div className="vehicle-image">
+              <img
+                src={
+                  num === 1
+                    ? v1
+                    : num === 2
+                      ? v2
+                      : v3
+                }></img>
+            </div>
             <h3>Vehicle {num}</h3>
             <p>High performance electric kart {num}</p>
           </div>
