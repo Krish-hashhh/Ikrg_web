cd14224254ab7babb4b5091e2bf279d3ce6a8791
NavyaGupta0301 <f20240850@pilani.bits-pilani.ac.in>
2025-08-02 12:30:10 +0530

    sponsor logo

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './Sponsors.css';
+import BKT from '../Assets/BKT.jpg';
+import ansys from '../Assets/ansys.png';
+import solidworks from '../Assets/solidworks.jpg';
+import NBC_logo from '../Assets/NBC_logo.png';
+
 
 export default function Sponsors() {
   return (
@@ -7,7 +12,18 @@ export default function Sponsors() {
       <h2>Our Sponsors</h2>
       <div className="sponsor-logos">
         {[1, 2, 3, 4].map(num => (
-          <div key={num} className="sponsor-logo">Logo {num}</div>
+          <div key={num} className="sponsor-logo">
+            <div className="sponsor-image">
+              <img
+                src={
+                  num === 1
+                    ? BKT
+                    : num === 2
+                      ? ansys
+                      : num ===3 ? NBC_logo : solidworks
+                }></img>
+            </div>
+          </div>
         ))}
       </div>
     </section>
